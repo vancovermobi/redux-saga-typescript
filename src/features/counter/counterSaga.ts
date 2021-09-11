e09adef2ba43964a3d7eb1605a6ab76bd590dbba
vancovermobi <vancovermobi@gmail.com>
2021-09-11 20:31:25 +0700

    06-takeEvery-takeLatest

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,17 +1,30 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { takeEvery } from "@redux-saga/core/effects";
-import { increment } from './counterSlice';
+import { delay, put, takeEvery, takeLatest } from "@redux-saga/core/effects";
+import { increment, incrementSaga, incrementSagaSucccess } from './counterSlice';
 
 
-export function* log(action: PayloadAction){
-    console.log('Log_takeEvery: ', action );
+// export function* log(action: PayloadAction){
+//     console.log('Log_takeEvery: ', action );
+// };
+export function* handleIncrementSaga(action: PayloadAction<number>){
+    // console.log('handleIncrementSaga ', action.payload );
+    console.log('Waiting 2s');
+
+    yield delay(2000);
+
+    console.log('Waiting done, dispatch action');
+
+    //Dispatch action success
+    yield put(incrementSagaSucccess(action.payload));
 };
 
 export default function* counterSaga(){
     console.log('Counter Saga');
 
     // Lắng nghe bất cứ 1 type dispatch action nào , mình sẽ log ra
-    yield takeEvery('*' , log ); 
-    yield takeEvery('counter/increment' , log ); 
-    yield takeEvery( increment().type , log ); 
+    // yield takeEvery('*' , log ); 
+    // yield takeEvery('counter/increment' , log ); 
+
+    yield takeEvery( incrementSaga.toString() , handleIncrementSaga );  
+    // yield takeLatest( incrementSaga.toString() , handleIncrementSaga );  
 }
